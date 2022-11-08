c6d1d783c427ab53f5d4d482a42d94c1bf16f7b5
danielmarquezalonso <daniel.marquez.alonso.al@iepsoligonosur.org>
2022-11-08 09:47:38 +0100

    Tienda base

--- tienda/admin.py
+++ tienda/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-#from .models import Marca
+from .models import Marca
+from .models import Producto
+from .models import Compra
 
-#admin.site.register(Marca)
+admin.site.register(Marca)
+admin.site.register(Producto)
+admin.site.register(Compra)
+
--- tienda/models.py
+++ tienda/models.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.db import models
 
-
 class Marca(models.Model):
 
     nombre = models.CharField(max_length=100)
@@ -16,19 +15,19 @@ class Producto(models.Model):
     modelo = models.CharField(max_length=100)
     precio = models.IntegerField()
     detalles = models.TextField(blank=True)
-    marca = models.ForeignKey(Marca, on_delete=models.CASCADE())
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
 
 
     def __str__(self):
         return self.nombre
 
 class Compra(models.Model):
-    fecha = models.DateField()
-    unidades = models.ForeignKey(Producto, on_delete=models.CASCADE())
+    fecha = models.DateTimeField()
+    unidades = models.ForeignKey(Producto, on_delete=models.CASCADE)
     importe = models.IntegerField()
 
     def __str__(self):
-        return self.fecha
+        return str(self.fecha)
 
 
 
