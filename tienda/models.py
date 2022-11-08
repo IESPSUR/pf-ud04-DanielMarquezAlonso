ec8defce712293c898173469a51c6fcf83554a19
danielmarquezalonso <daniel.marquez.alonso.al@iepsoligonosur.org>
2022-11-08 09:01:19 +0100

    Migration no funciona

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,37 @@
+from django.conf import settings
+from django.db import models
+
+
+class Marca(models.Model):
+
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    unidades = models.IntegerField()
+    modelo = models.CharField(max_length=100)
+    precio = models.IntegerField()
+    detalles = models.TextField(blank=True)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE())
+
+
+    def __str__(self):
+        return self.nombre
+
+class Compra(models.Model):
+    fecha = models.DateField()
+    unidades = models.ForeignKey(Producto, on_delete=models.CASCADE())
+    importe = models.IntegerField()
+
+    def __str__(self):
+        return self.fecha
+
+
+
 from django.db import models
 
 # Create your models here.
