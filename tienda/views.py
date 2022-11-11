4dd37995fe49848ff051b1745d809a40d34b5dd9
danielmarquezalonso <daniel.marquez.alonso.al@iepsoligonosur.org>
2022-11-11 12:51:30 +0100

    Listado Productos

--- tienda/views.py
+++ tienda/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
+
+from tienda.models import Marca
+
 
 # Create your views here.
 def welcome(request):
     return render(request,'tienda/index.html', {})
+
+def listado(request):
+    marca = Marca.objects.all()
+    return render(request,'tienda/admin/listado.html', { 'marca' : marca })
+
+
+
+
+
+
