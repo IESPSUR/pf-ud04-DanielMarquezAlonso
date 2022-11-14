de5d3afe38985143abc6f8ff57db098f0860e7a4
danielmarquezalonso <daniel.marquez.alonso.al@iepsoligonosur.org>
2022-11-14 10:28:08 +0100

    Detalle de producto muestra y estructura del views

--- tienda/views.py
+++ tienda/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
+from tienda.forms import ProductoForm
 from tienda.models import Producto
 
 
@@ -12,8 +13,17 @@ def admin_listado(request):
     producto = Producto.objects.all()
     return render(request,'tienda/admin/listado.html', { 'productos' : producto })
 
-def admin_producto_detalle(request):
-    return render(request,'tienda/admin/producto_detalle.html')
+def admin_producto_detalle(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    if request.method == "POST":
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            producto = form.save(commit=False)
+            producto.save()
+
+    producto_form = ProductoForm(instance=producto)
+
+    return render(request,'tienda/admin/producto_detalle.html', {'producto_form' : producto_form})
 
 def admin_producto_nuevo(request):
     return render(request,'tienda/admin/producto_detalle.html')
